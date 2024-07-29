4bd41fabf40a61492fe3516a4570767ea7f80487
LeonidasVs <villafrancosibrian@gmail.com>
2024-07-29 09:07:47 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto C#/ConexionSQL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_C_
+{
+    public partial class ConexionSQL : Form
+    {
+        public ConexionSQL()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+            idtext.Enabled = false;
+
+        }
+
+        private void ConexionSQL_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwindDataSet.Customers' Puede moverla o quitarla según sea necesario.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 ir = new Form1();
+            ir.ShowDialog();
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            idtext.Enabled = true;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            idtext.Enabled = false; 
+        }
+    }
+}
